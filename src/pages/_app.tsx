415b5ec935cb3e2000add9816865d6b1c2ef2298
Mário César Silva <mcgato33@hotmail.com>
2021-06-06 05:38:18 -0100

    styled components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { AppProps } from "next/app";
+
+import { GlobalStyle } from "../styles/globals";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <> 
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
