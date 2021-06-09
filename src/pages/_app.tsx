49610ec02e8f02a54de6b85e94eae35cdc439200
Mário César Silva <mcgato33@hotmail.com>
2021-06-09 14:44:20 -0100

    layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
 import { AppProps } from "next/app";
 
-import { GlobalStyle } from "../styles/globals";
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <> 
-      <GlobalStyle />
       <Component {...pageProps} />
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,63 @@
+import Head from "next/head";
+
+import { GiPadlock } from "react-icons/gi";
+
+import styles from "../styles/pages/home.module.scss";
+
 export default function Home() {
   return (
-    <div>
-      <h1>ola, mundo!</h1>
-    </div>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <h2 >Sign in to your account</h2>
+          </div>
+          
+          <form>
+            <div className={styles.myInput}>
+              <input 
+                name="email" 
+                type="email" 
+                placeholder="E-mail"
+              />
+              <input 
+                name="password" 
+                type="password" 
+                placeholder="Senha" 
+              />
+            </div>
+
+            <div className={styles.forgot}>
+              <div>
+                <input
+                  id="remember_me"
+                  name="remember_me"
+                  type="checkbox"
+                />
+                <label htmlFor="remember_me">
+                  Remember me
+                </label>
+              </div>
+
+              <div>
+                <a href="#">
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div className={styles.myButton}>
+              <button type="submit">
+                <span><GiPadlock /></span>
+                <strong>Sign in</strong> 
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
